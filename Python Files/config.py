25bd149022289658bbdd18c894422523b6b32972
Vishal Gupta <vgupta1@mit.edu>
2013-11-21 08:59:49 -0500

    some fixes to Nominal

---
+++ Python Files/config.py
@@ -0,0 +1,7 @@
+""" Config Constants for the UC Problem """
+
+FUEL_TYPES = ("Steam", "CT", "CC", "Diesel", "Hydro", "Nuclear", "Wind", "FixedImport")
+HORIZON_LENGTH = 24
+EPSILON_ZERO = 0.
+PENALTY = 5
+TOL = 1e-5
