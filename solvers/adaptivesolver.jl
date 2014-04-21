f39b0b6ed4a0425545c90c3d5dbf3419ed720056
vgupta1 <vgupta1@mit.edu>
2014-04-21 11:29:52 -0400

    Adding Support for Seeding CutGen

--- solvers/adaptivesolver.jl
+++ solvers/adaptivesolver.jl
@@ -56,41 +56,23 @@ type UCAff
 
     sheds ##Vector{Variable}  Simply variables for now, revisit
     uncs  
-    proj_fcn  # hr -> projMatrix
+    proj_fcn  #hr -> projMatrix
 
-    warmstart
+	#may or may not be set by user
+    warmstart  
+    sample_uncs
 
     UCAff(robmodel, gens, penalty, uncs) =new(robmodel, gens, penalty, 
     		Dict{String, Any}(), Dict{String, Any}(), Dict{String, Any}(), #ons starts stops 
     		Dict{String, Any}(), Dict{String, Any}(), Dict{String, Any}(),  #prod, varcost, maxprod
             Dict{String, Any}(), Dict{String, Any}(), #prodcoeff, prodintercept
-            Variable[], uncs, nothing, nothing)  #sheds, uncs, proj_fcn, warmstart
+            Variable[], uncs, nothing, nothing, nothing)  #sheds, uncs, proj_fcn, warmstart
 end
 #VG Add a better convenience constructor
 
 solve_(aff::UCAff, report; args...) = JuMPeR.solveRobust(aff.m, report=report; args...)
 isBaseLoad(g) = g.fueltype == "Nuclear"
-
-function addSecondStage!(aff::UCAff; TOL=1e-8)
-    for g in values(aff.gendata)
-		gn = g.name
-		if isBaseLoad(g)
-			ps = [addProdVar!(aff.m, aff.ons[gn][ihr], g.ecomin[ihr], g.ecomax[ihr], TOL) for ihr=1:HRS]
-			aff.prodcoeff[gn]    = [ [] for ihr = 1:HRS ]
-			aff.prodint[gn] = ps
-			aff.prod[gn]          = ps
-			aff.maxprod[gn]       = ps
-		else
-	    	jumble = [addAdaptProdVar!(aff.m, aff.proj_fcn(ihr), aff.uncs, aff.ons[gn][ihr], 
-        								g.ecomin[ihr], g.ecomax[ihr], TOL) for ihr=1:HRS]
-    		aff.prodcoeff[gn], aff.prodint[gn], aff.prod[gn], aff.maxprod[gn] = zip(jumble...)
-    	end
-
-		#This works because everything is disaggregated for now
-        blocks, relcosts = getCurve(g)
-        aff.varcosts[gn] = [addVarCost!(aff.m, aff.maxprod[gn][ihr], blocks, relcosts) for ihr = 1:HRS]
-    end
-end
+addSampleUncs!(aff::UCAff, samples) = aff.sample_uncs = copy(samples)
 
 function addWarmStart!(aff::UCAff; force=false)
 	if force
@@ -106,11 +88,15 @@ function addWarmStart!(aff::UCAff; force=false)
 			setValue(aff.ons[gn][ihr],    w.ons[gn][ihr])
 			setValue(aff.starts[gn][ihr], w.starts[gn][ihr])
 			setValue(aff.stops[gn][ihr],  w.stops[gn][ihr])
-			map(x ->setValue(x, 0.0), aff.prodcoeff[gn][ihr])
-			if typeof(aff.prodint[gn][ihr]) == Variable
-				setValue(aff.prodint[gn][ihr], w.prod[gn][ihr])
-			end
+			map(x ->setValue(x, 0.0), aff.prodcoeff[gn][ihr])  #if empty, do nothing
 			setValue(aff.varcosts[gn][ihr], w.varcosts[gn][ihr])
+
+			#VG This sort of embedded logic about baseload and trivial elements is yucky
+			if isa(aff.prodint[gn][ihr], Variable)
+				setValue(aff.prodint[gn][ihr], w.prod[gn][ihr])
+			elseif isa(aff.prod[gn][ihr], Variable)
+				setValue(aff.prod[gn][ihr], w.prod[gn][ihr])
+			end
 		end
 	end
 	for ihr = 1:HRS
@@ -123,21 +109,25 @@ function forceWarmStart!(aff::UCAff)
 	if w == nothing
 		return
 	end
-	for gname in keys(aff.gendata)
+	for gn in keys(aff.gendata)
 		for ihr = 1:HRS
 			# ons, starts and stops
-			@addConstraint(aff.m, aff.ons[gname][ihr]    == int(w.ons[gname][ihr]))
-			@addConstraint(aff.m, aff.starts[gname][ihr] == int(w.starts[gname][ihr]))
-			@addConstraint(aff.m, aff.stops[gname][ihr]  == int(w.stops[gname][ihr]))
+			@addConstraint(aff.m, aff.ons[gn][ihr]    == int(w.ons[gn][ihr]))
+			@addConstraint(aff.m, aff.starts[gn][ihr] == int(w.starts[gn][ihr]))
+			@addConstraint(aff.m, aff.stops[gn][ihr]  == int(w.stops[gn][ihr]))
+			@addConstraint(aff.m, aff.varcosts[gn][ihr] == w.varcosts[gn][ihr])
 
 			# prods and varcosts
-			for ix = 1:length(aff.prodcoeff[gname][ihr])
-				@addConstraint(aff.m, aff.prodcoeff[gname][ihr][ix] == 0.0 )
+			for ix = 1:length(aff.prodcoeff[gn][ihr])
+				@addConstraint(aff.m, aff.prodcoeff[gn][ihr][ix] == 0.0 )
 			end
-			if aff.prodint[gname][ihr] != nothing
-				@addConstraint(aff.m, aff.prodint[gname][ihr] == w.prod[gname][ihr])
+
+			#VG This sort of embedded logic about baseload and trivial elements is yucky
+			if isa(aff.prodint[gn][ihr], Variable)
+				@addConstraint(aff.m, aff.prodint[gn][ihr] ==  w.prod[gn][ihr])
+			elseif isa(aff.prod[gn][ihr], Variable)
+				@addConstraint(aff.m, aff.prod[gn][ihr]==  w.prod[gn][ihr])
 			end
-			@addConstraint(aff.m, aff.varcosts[gname][ihr] == w.varcosts[gname][ihr])
 		end
 	end
 	for ihr = 1:HRS
@@ -145,50 +135,109 @@ function forceWarmStart!(aff::UCAff)
 	end
 end
 
-function addAdaptProdVar!(m, projMatrix, uncs, x, min, max, TOL)
-	if max - min < TOL
-		#A trivial case where adaptivity non-helpful
-		return [], nothing, max * x, max * x
-	end
-	k = size(projMatrix, 1)
-	@defVar(m, prods[1:k])
-	@defVar(m, prods0 >= 0)
-	@defVar(m, maxprod >=0)
-	proj_uncs = projMatrix * uncs[:]
-	p = sum([proj_uncs[ix] * prods[ix] for ix = 1:k]) + prods0 
-    
-    addConstraint(m, p - maxprod <= 0)
+function addSecondStage!(aff::UCAff; TOL=1e-8)
+    for g in values(aff.gendata)
+		gn = g.name
+		if isBaseLoad(g)
+			ps = [addProdVar!(aff.m, aff.ons[gn][ihr], g.ecomin[ihr], g.ecomax[ihr], TOL) for ihr=1:HRS]
+			aff.prodcoeff[gn]     = [ [] for ihr = 1:HRS ]
+			aff.prodint[gn]       = [nothing for ihr = 1:HRS]
+			aff.prod[gn]          = [1 * ps[ihr] for ihr = 1:HRS] #make it an affine expression
 
-    @addConstraint(m, maxprod <= max * x)
-	addConstraint(m, p - min * x >= 0)
+			#for type consistency, make maxprods a variable
+			@defVar(aff.m, maxprods[1:HRS] >= 0)
+			for ihr= 1:HRS
+				@addConstraint(aff.m, ps[ihr] <= maxprods[ihr])
+			end
+			aff.maxprod[gn] = maxprods
+		else
+	    	jumble = [addAdaptProdVar!(aff.m, aff.proj_fcn(ihr), aff.uncs, aff.ons[gn][ihr], 
+        								g.ecomin[ihr], g.ecomax[ihr], TOL) for ihr=1:HRS]
+    		aff.prodcoeff[gn], aff.prodint[gn], aff.prod[gn], aff.maxprod[gn] = zip(jumble...)
+    		
+    		#if there are samples, add them now
+    		if aff.sample_uncs != nothing
+	    		for indx = 1:size(aff.sample_uncs, 1)
+	    			for ihr = 1:HRS
+	    				addEcoMinMax!(aff.m, aff.proj_fcn(ihr), aff.sample_uncs[indx, :], 
+	    								aff.ons[gn][ihr], g.ecomin[ihr], aff.maxprod[gn][ihr], 
+	    								aff.prodcoeff[gn][ihr], aff.prodint[gn][ihr])
+	    			end
+	    		end
+	    	end
+    	end
 
-    #VG CLUDGE add these for safety in cutting plane
-    @addConstraint(m, prods0 <= maxprod)
-    @addConstraint(m, prods0 >= min * x)
-
-	return prods, prods0, p, maxprod
+		#This works because everything is disaggregated for now
+        blocks, relcosts = getCurve(g)
+        aff.varcosts[gn] = [addVarCost!(aff.m, aff.maxprod[gn][ihr], blocks, relcosts) for ihr = 1:HRS]
+    end
 end
 
-function addLoadBalance!(aff::UCAff, forecasts, split_fcn = nothing)
-	(split_fcn != nothing) && error("Splitting Loads not yet Implemented")
+function addEcoMinMax!(m, projMatrix, uncs, x, min, maxprod, prodcoeff, prods0)
+	k = length(prodcoeff)  
+	if k == 0  #Not affine adaptive
+		return prods0
+	else
+		proj_uncs = projMatrix * uncs[:]
+		# println("Sizes: ", size(projMatrix), " ", size(uncs[:]), " ", size(proj_uncs[:]))
+		prod = sum([proj_uncs[ix] * prodcoeff[ix] for ix = 1:k]) + prods0 
+	    addConstraint(m, prod - maxprod <= 0)
+		addConstraint(m, prod - min * x >= 0)
+		return prod
+	end
+end
+
+function addAdaptProdVar!(m, projMatrix, uncs, x, min, max, TOL)
+	@defVar(m, maxprod >=0)
+    @addConstraint(m, maxprod <= max * x)
+
+	if max - min < TOL
+		#A trivial case where adaptivity non-helpful
+		return [], nothing, max * x, maxprod
+	end
+	k = size(projMatrix, 1)
+	@defVar(m, prodcoeff[1:k])
+	@defVar(m, prodint >= 0)
+	prod = addEcoMinMax!(m, projMatrix, uncs, x, min, maxprod, prodcoeff, prodint)
+	return prodcoeff, prodint, prod, maxprod
+end
+
+function addLoadBalance!(aff::UCAff, forecasts)
 	uncs = aff.uncs
     for ihr = 1:length(forecasts)
-        total_prod = sum([aff.prod[gname][ihr] for gname in keys(aff.gendata)])
+        total_prod = sum([aff.prod[gn][ihr] for gn in keys(aff.gendata)])
         @defVar(aff.m, shed >= 0)
         addConstraint(aff.m, total_prod - shed - uncs[ihr]  <= forecasts[ihr])
         addConstraint(aff.m, total_prod + shed - uncs[ihr]  >= forecasts[ihr])
         push!(aff.sheds, shed)
 
-        #VG cludges are a real thing
-        total_prod0 = AffExpr()
-        for gname in keys(aff.gendata)
-        	if aff.prodint[gname][ihr] != nothing
-        		total_prod0 += aff.prodint[gname][ihr]
+        # Add the sampled points, if they exist
+        # VG Factor this out somehow
+        if aff.sample_uncs != nothing
+	        projMatrix = aff.proj_fcn(ihr)
+	    	for ix = 1:size(aff.sample_uncs, 1)
+        		# form the value of the affine value here
+        		total_prod = AffExpr()
+        		proj_uncs = projMatrix * aff.sample_uncs[ix, :]'
+        		for gn in keys(aff.gendata)
+	    			for jx = 1:length(aff.prodcoeff[gn][ihr])  #if non-affine, empty loop
+	    				push!(total_prod, proj_uncs[jx], aff.prodcoeff[gn][ihr][jx])
+	    			end
+	    			#VG Yucky logic
+	    			if aff.prodint[gn][ihr] != nothing 
+	    				push!(total_prod, 1., aff.prodint[gn][ihr])
+	    			elseif isa(aff.prod[gn][ihr], Variable) #robust policy
+	    				push!(total_prod, 1., aff.prod[gn][ihr])
+	    			else  #a trivial min=max case
+	    				append!(total_prod, aff.prod[gn][ihr]) 
+	    			end
+        		end
+        		# add the sampled cnsts
+		        @addConstraint(aff.m, total_prod - shed - aff.sample_uncs[ix, ihr]  <= forecasts[ihr])
+		        @addConstraint(aff.m, total_prod + shed - aff.sample_uncs[ix, ihr]  >= forecasts[ihr])
         	end
         end
-        @addConstraint(aff.m, total_prod0 - shed  <= forecasts[ihr])
-        @addConstraint(aff.m, total_prod0 + shed  >= forecasts[ihr])
     end
-    addWarmStart!(aff)  ##VG This should live somewhere else...
+    addWarmStart!(aff)  ##VG Move this to main with a try-catch
 end
 
--- solvers/affTiming.jl
+++ solvers/affTiming.jl
@@ -8,7 +8,7 @@ include("robustsolver.jl")
 include("UncSets.jl")
 include("adaptivesolver.jl")
 
-gens, scaling       = loadISO("../Data/AndysGenInstance", .1)
+gens, scaling       = loadISO("../Data/AndysGenInstance", 1)
 dts, vals           = readLoads("../Data/ISO-NE Load Data/PredTest.csv")
 dts_true, vals_true = readLoads("../Data/ISO-NE Load Data/LoadTest.csv")
 vals               *= scaling
@@ -21,25 +21,65 @@ Gamma2              = 4  * scaling * scaling
 eps                 = .1
 ##################################
 #solve the robust problem for a warmstart
-m = RobustModel(solver=GurobiSolver())
-alphas, uncs = createPolyUCS(m, resids, Gamma1, Gamma2, kappa(eps))
-rob = UCRob(m, gens, penalty, uncs)
-solve(rob, vals[int(ARGS[1]), :], report=true)
+# m = RobustModel(solver=GurobiSolver())
+# alphas, uncs = createPolyUCS(m, resids, Gamma1, Gamma2, kappa(eps))
+# rob = UCRob(m, gens, penalty, uncs)
+# solve(rob, vals[int(ARGS[1]), :], report=true)
 
-w = WarmStartInfo()
-copyWarmStart(rob, w)
+# w = WarmStartInfo()
+# copyWarmStart(rob, w)
 
 ##################################
 ## a test function
+#INDXSET = [72 88 107 160]
 function testRun()
-	rm2 = RobustModel(solver=GurobiSolver(MIPGap=5e-3), cutsolver=GurobiSolver(OutputFlag=0))  #MIPGap=1e-2, OptimalityTol=1e-4)
+	rm2 = RobustModel(solver=GurobiSolver(MIPGap=1e-3, OutputFlag=1), cutsolver=GurobiSolver(OutputFlag=0))  #MIPGap=5e-3
 	alphas, uncs = createPolyUCS(rm2, resids, Gamma1, Gamma2, kappa(eps));
 	aff = UCAff(rm2, gens, penalty, uncs);
 	aff.proj_fcn = eigenProjMatrixData(resids, 1)
-	aff.warmstart = w
+	# aff.warmstart = w
+
+	# read in the starting cuts
+	if length(ARGS) >= 3
+		println("Adding Old Cuts")
+		samples = readdlm(open(ARGS[3], "r"), '\t')
+		aff.sample_uncs = samples
+	end
 	solve(aff, vals[int(ARGS[1]), :], report=true, usebox=false, 
-				prefer_cuts= (ARGS[2]=="true") ) #, lprelax=true
+				prefer_cuts=true,  active_cuts=(ARGS[2]=="true")) 
+
+	println( "Active Cuts:")
+	ac = rm2.ext[:Robust].activecuts
+	println(" Total: $(length(ac))")
+	ac = unique(ac)
+	println(" Unique: $(length(ac))")
+
+	# # ofile = open(ARGS[3], "w")
+	# # for ix = 1:length(ac)
+	# # 	writedlm(ofile, transpose(ac[ix][1:HRS]) )  #this is a dangeorus hack
+	# # end
+	# close(ofile)
 end
+
 
 println( "\n Robust Solve Begins Here \n ")
 println( @elapsed testRun() )
+
+
+# ## Load it up again and resolve
+# rm2 = RobustModel(solver=GurobiSolver(OutputFlag=0, MIPGap=1e-3), cutsolver=GurobiSolver(OutputFlag=0))  #MIPGap=5e-3
+# alphas, uncs = createPolyUCS(rm2, resids, Gamma1, Gamma2, kappa(eps));
+# aff = UCAff(rm2, gens, penalty, uncs);
+# aff.proj_fcn = eigenProjMatrixData(resids, 1)
+
+# #read in the starting cuts
+# if length(ARGS) >= 3
+# 	samples = readdlm(open(ARGS[3], "r"), '\t')
+# 	aff.sample_uncs = samples
+# end
+
+# solve(aff, vals[int(ARGS[1]), :], report=true, usebox=false, 
+# 			prefer_cuts=true,  active_cuts=(ARGS[2]=="true")) 
+
+
+
