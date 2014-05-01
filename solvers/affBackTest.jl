caf3aa79f1f6ff44a919af2de17599e0f0b79ead
vgupta1 <vgupta1@mit.edu>
2014-04-30 20:35:13 -0400

    Streamling AFfine Backtest

--- solvers/affBackTest.jl
+++ solvers/affBackTest.jl
@@ -25,30 +25,22 @@ penalty             = 5e3
 file_out            = fileout = open(ARGS[1], "a")
 
 numEigs             = 1
-Gamma1              = 0.5171897 * scaling
-Gamma2              = 4.190187   * scaling * scaling
+Gamma1              = .14   #From rough jacknife analysis
+Gamma2              = .27
 kappa                = sqrt(1/.1 - 1)
-GammaBS             = 1.282 * sqrt(24)
+
+GammaBS             = 1.282 * sqrt(24)  #These are guestimated.
 GammaBound          = 3.0
 
-clustermap = readdlm("../results/All/ValidateSet/validateClusterMap2.csv", '\r')
-println(size(clustermap))
-clustermap = [int(c) for c in clustermap]
-println(size(clustermap))
+box_size            =.9333  #From 5-fold crossvalidation
 
 for iRun = 1:length(dts)
-    if iRun < 75
-        continue  #because we ran once before
-    end
-
     if iRun == 1
         writedlm(file_out, ["Date" "Method" "status" "Time" "FixedCost" "PredVarCost" "VarCost" "PredShed" "Shed"])
         println(["Date" "Method" "status" "Time" "FixedCost" "PredVarCost" "VarCost" "PredShed" "Shed"])
     end
 
-    cluster = clustermap[iRun]
-
-    #solve the nominal problem to use as variance reduction
+    #Nominal problem
     m = RobustModel(solver=GurobiSolver(OutputFlag=0, MIPGap=1e-3, TimeLimit=60*15))
     nom = UCNom(m, gens, penalty)
     solvetime = 0; tic()
@@ -56,30 +48,28 @@ for iRun = 1:length(dts)
     solvetime = toq()
     nom2 = secondSolve(nom, vals_true[iRun, :], report=false)
     writedlm(file_out, [dts[iRun] "Nominal" status solvetime getStartCost(nom) getVarCost(nom) getVarCost(nom2) totShed(nom) totShed(nom2)])
-    println( [dts[iRun] "Nominal" status solvetime getStartCost(nom) getVarCost(nom) getVarCost(nom2) totShed(nom) totShed(nom2)])
+    println([dts[iRun] "Nominal" status solvetime getStartCost(nom) getVarCost(nom) getVarCost(nom2) totShed(nom) totShed(nom2)])
     flush(file_out)
 
-    #Solve a robust problem for warm start info
-    #VG This should be change to properly use a warm start file
-    # This biases the timings  a little.
-    rm = RobustModel(solver=GurobiSolver(OutputFlag=0, MIPGap=1e-2, TimeLimit=60*15))
-    uncs = createUM(rm, resids, .9333)
+    #Robust problem
+    rm = RobustModel(solver=GurobiSolver(OutputFlag=0, MIPGap=1e-3, TimeLimit=60*15))
+    uncs = createUM(rm, resids, box_size)
     rob    = UCRob(rm, gens, penalty, uncs)
-    solve(rob, vals[iRun, :], usebox=false, report=false, prefer_cuts=true)
+    solvetime = 0; tic()
+    status = solve(rob, vals[iRun, :], usebox=false, report=false, prefer_cuts=true)
+    solvetime = toq()
     w = copyWarmStart(rob, WarmStartInfo())
+    rob2 = secondSolve(rob, vals_true[iRun, :], report=false)
+    writedlm(file_out, [dts[iRun] "Robust" status solvetime getStartCost(rob) getVarCost(rob) getVarCost(rob2) totShed(rob) totShed(rob2)])
+    println([dts[iRun] "Robust" status solvetime getStartCost(rob) getVarCost(rob) getVarCost(rob2) totShed(rob) totShed(rob2)])
+    flush(file_out)
 
-    println("Got through Robust")
-
-
-    #Solve against UCS
+    #UCS
     rm2 = RobustModel(solver=GurobiSolver(MIPGap=1e-3, OutputFlag=0, Method=3, TimeLimit=60*30), 
                      cutsolver=GurobiSolver(OutputFlag=0))
     alphas, uncs = createPolyUCS(rm2, resids, Gamma1, Gamma2, kappa, true)
     aff = UCAff(rm2, gens, penalty, uncs)
     aff.proj_fcn = eigenProjMatrixData(resids, numEigs)
-
-    ##VG Change this to use proper cluster map
-#    aff.sample_uncs = readdlm(open("../results/Size_10/TestSet/cuts$cluster.txt", "r"), '\t')
     aff.warmstart = w
 
     try
@@ -88,22 +78,19 @@ for iRun = 1:length(dts)
         solvetime = toq()
         aff2     = secondSolve(aff, vals_true[iRun, :], report=false)
         writedlm(file_out, [dts[iRun] "UCS" status solvetime getStartCost(aff) getVarCost(aff) getVarCost(aff2) totShed(aff) totShed(aff2)])
-        # println( file_out, [dts[iRun] "UCS" status solvetime getStartCost(aff) getVarCost(aff) getVarCost(aff2) totShed(aff) totShed(aff2)])
+        println([dts[iRun] "UCS" status solvetime getStartCost(aff) getVarCost(aff) getVarCost(aff2) totShed(aff) totShed(aff2)])
         flush(file_out)
     catch e
             show(e)
     end
 
     #Solve against UB
-    rm2 = RobustModel(solver=GurobiSolver(MIPGap=1e-3, OutputFlag=1, Method=3, TimeLimit=30*60), 
+    rm2 = RobustModel(solver=GurobiSolver(MIPGap=1e-3, OutputFlag=0, Method=3, TimeLimit=30*60), 
                       cutsolver=GurobiSolver(OutputFlag=0))
     alphas, uncs = createBertSimU(rm2, mean(resids, 1), cov(resids), GammaBS, GammaBound, false)
     aff = UCAff(rm2, gens, penalty, uncs);
     aff.proj_fcn = identProjMatrixData(resids, numEigs)
-
-#    aff.sample_uncs = readdlm(open("../results/Size_10/TestSet/budgetcut$cluster.txt", "r"), '\t')    
     aff.warmstart = w
-
 
     try
         solvetime = 0; tic()
